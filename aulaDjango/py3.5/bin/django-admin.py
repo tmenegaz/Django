bbd70906949e87113c830dc00ec3da2c27551143
tmenegaz <tmenegaz77@gmail.com>
2016-10-11 13:35:52 -0300

    projeto aula1: início

---
+++ aulaDjango/py3.5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tmenegaz/Documentos/cimatec/2016.2/escolaTecnica/mundoSenai/django/aula/aulaDjango/py3.5/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
