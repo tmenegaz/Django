1be5225f8e22c257e46e8b1f96419e3f11cb24dd
tmenegaz <tmenegaz77@gmail.com>
2016-10-24 16:23:16 -0300

    atualizado

--- aulaDjango/py3.5/aula3/aula3/settings.py
+++ aulaDjango/py3.5/aula3/aula3/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # meus app em templates
+    'aula3',
 ]
 
 MIDDLEWARE_CLASSES = [
